da59fbba2f57fec10a644d1eb8622cc1e8f0f7f9
PC-300 <lmcomputer@naver.com>
2024-10-15 09:54:30 +0900

    exam13추가

---
+++ exam13/Test01.java
@@ -0,0 +1,8 @@
+package exam13;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ exam14/test01.java
@@ -0,0 +1,7 @@
+package exam14;
+
+public class test01 {
+    public static void main(String[] args) {
+
+    }
+}
